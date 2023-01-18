c73d85e7ae8584f24d045e9997f796c948636e03
IreneZoi <irene.zoi@cern.ch>
2023-01-18 18:49:31 +0100

    first go to 2016 with aQGC

---
+++ Configurations/VBSjjlnu/Full2016v7/conf_fit_v4.5_aQGC/structure_split.py
@@ -0,0 +1,176 @@
+# structure configuration for datacard
+from itertools import product, chain
+#structure = {}
+
+wjets_bins = []
+for ir in range(1,22):
+    wjets_bins.append("Wjets_res_"+str(ir))
+for ir in range(1,8):
+    wjets_bins.append("Wjets_boost_"+str(ir))
+
+
+phase_spaces_boost = [c for c in cuts if "boost" in c]
+phase_spaces_res = [c for c in cuts if "res" in c]
+
+
+for wbin in wjets_bins:
+    if 'boost' in wbin:
+        structure[wbin] = {
+                    'isSignal' : 0,
+                    'isData'   : 0 ,
+                    'removeFromCuts': phase_spaces_res 
+        }
+    else:
+        structure[wbin] = {
+                    'isSignal' : 0,
+                    'isData'   : 0 ,
+                    'removeFromCuts': phase_spaces_boost 
+        }
+
+
+# structure['Wjets_HT']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0
+#               }
+
+structure['DY']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+structure['top']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+# structure['VV']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0
+#               }
+for VV_s in VV_samples:
+    structure[VV_s]  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+structure['Fake']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+
+structure['VVV']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+# structure['VBF-V']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0 
+#               }
+
+structure['VBF-V_dipole']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['ggWW']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['Vg']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VgS']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+# structure['VBS_WZll']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0 
+#               }
+
+# structure['VBS_ZZ']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0 
+#               }
+
+
+# structure['VBS']  = { 
+#                   'isSignal' : 1,
+#                   'isData'   : 0 
+#               }
+
+# structure['VBS_dipoleRecoil']  = { 
+#                   'isSignal' : 1,
+#                   'isData'   : 0 
+#               }
+
+# structure['VBS_osWW']  = {  
+#                   'isSignal' : 1,
+#                   'isData'   : 0 
+#               }
+
+# structure['VBS_ssWW']  = {  
+#                   'isSignal' : 1,
+#                   'isData'   : 0 
+#               }
+
+# structure['VBS_WZjj']  = {  
+#                   'isSignal' : 1,
+#                   'isData'   : 0 
+#               }
+
+# Structure for aqgc
+
+structure['sm'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+#### EFT ###
+structure['quad_cT0'] = {
+                 'isSignal' : 1,
+                 'isData'   : 0
+                 }
+structure['sm_lin_quad_cT0'] = {
+                 'isSignal' : 1,
+                 'isData'   : 0
+                 }
+
+# structure['quad_cT2'] = {
+#                  'isSignal' : 1,
+#                  'isData'   : 0
+#                  }
+# structure['sm_lin_quad_cT2'] = {
+#                  'isSignal' : 1,
+#                  'isData'   : 0
+#                  }
+
+
+# data
+
+
+structure['DATA']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 1 
+              }
+
+
+# It seems that this is needed for 2016 comparing the rate parameters that I can reproduce with what is in the full run 2 datacard from Davide /eos/user/d/dvalsecc/www/VBSPlots/FullRun2/datacards/datacards_combined/fullrun2_fit_v4.5.5/run2_all/combined_run2_all.txt
+for sample in structure.keys():
+    if sample.find("Fake") != -1:
+        # Correct the luminosity
+        structure[sample]['scaleSampleForDatacard'] = 36.33/35.867
+
+# It seems that this is not needed anymore for 2016 comparing the rate parameters that I can reproduce with what is in the full run 2 datacard from Davide /eos/user/d/dvalsecc/www/VBSPlots/FullRun2/datacards/datacards_combined/fullrun2_fit_v4.5.5/run2_all/combined_run2_all.txt
+'''
+for sample in structure.values():
+    if sample['isData'] != 1:
+        # Correct the luminosity
+        sample['scaleSampleForDatacard'] = 36.33/35.867
+'''
---
+++ Configurations/VBSjjlnu/Full2016v7/configuration_fit_v4.5_2016_split_aQGC.py
@@ -0,0 +1,46 @@
+# example of configuration file
+treeName= 'Events'
+
+tag = 'fit_v4.5_2016_split_aQGC_cT0_newsignals'
+direc = 'conf_fit_v4.5_aQGC'
+
+# used by mkShape to define output directory for root files
+outputDir = '/eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/rootFile_'+tag 
+
+# file with TTree aliases
+aliasesFile = direc+'/aliases_split.py'
+
+# file with list of variables
+variablesFile = direc+'/variables.py'
+
+# file with list of cuts
+cutsFile = direc+'/cuts.py'
+
+# file with list of samples
+samplesFile = direc+'/samples_split_aQGC.py' 
+
+# file with list of samples
+plotFile = direc+'/plot_split.py'
+
+
+
+# luminosity to normalize to (in 1/fb)
+lumi = 36.33
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plot_'+tag 
+# used by mkDatacards to define output directory for datacards
+
+outputDirDatacard = 'datacards_' +tag
+
+# structure file for datacard
+structureFile = direc+'/structure_split.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+#nuisancesFile = direc+'/nuisances_datacard_split.py'
+nuisancesFile = direc+'/nuisances.py'
+#nuisancesFile = direc+'/nuisances_aQGC.py'
+
+customizeScript = direc + '/customize.py'
