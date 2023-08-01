66972035f49b0e3d937b96a4e2851f4a45e3af3b
IreneZoi <irene.zoi@cern.ch>
2023-08-01 15:39:40 +0200

    postfit scripts

--- Configurations/VBSjjlnu/datacards/postfit_loop_Run2_EFT_WV_env.sh
+++ Configurations/VBSjjlnu/datacards/postfit_loop_Run2_EFT_WV_env.sh
@@ -10,13 +10,15 @@
     #
     ###############################################################################################################
 
+Category=$1 #boost
+CUT=$2 #boost_sig_ele
+PLOTVAR=$3 #Mww_binzv
+YEAR=$4
+SRVAR=$5
 
-Date2016=23May2022_2016
-Date2017=29May2022_2017
-Date2018=8June2022_2018
 
 # datacardDir=2018_fit_v4.5.5_aQGC_cT0_full_DNN
-datacardDir=2018_fit_v4.5.5_aQGC_cT0_eboliv2_full
+datacardDir=2018_fit_v4.5.5_aQGC_cT0_eboliv2_full_${SRVAR}
 # datacardDir=fullrun2_fit_v4.5.5_aQGC_cT0_DNN #MwwDav/ #vbsmjj/ #DNN/
 #datacardDir2=Full2081v7/datacards_fit_v4.5_2018_split_aQGC_cT0_NoVBS_WithSignalNuis/ #Mww20/
 #datacardDir2=Full2016v7/datacards_fit_v4.5_2016_split_aQGC_cT0/ #Mww20/
@@ -37,23 +39,20 @@ echo full operator "$fulloperator"
 #DATACARD_FIT=/afs/cern.ch/work/i/izoi/VBSanalysis/CMSSW_11_1_4/src/PlotsConfigurations/Configurations/VBSjjlnu/datacards/${datacardDir}/run2_boost/combined_run2_boost_postfitRateParam2017 #2018_boost_split_Dipole_v4.5/combined_2018_boost_split_Dipole_v4.5
 DATACARD_FIT=/afs/cern.ch/work/i/izoi/VBSanalysis/CMSSW_11_1_4/src/PlotsConfigurations/Configurations/VBSjjlnu/datacards/${datacardDir}/2018_all_split_Dipole_v4.5/combined_2018_all_split_Dipole_v4.5
 
-# source postfit_loop_Run2_EFT_WV.sh boostonly boost_sig_ele Mww_binzv
-# source postfit_loop_Run2_EFT_WV.sh boostonly boost_sig_ele mjj_vbs
-# source postfit_loop_Run2_EFT_WV.sh boostonly boost_sig_ele Mww
-# source postfit_loop_Run2_EFT_WV.sh boostonly boost_wjetcr_ele DNNoutput_boost
-# source postfit_loop_Run2_EFT_WV.sh boostonly boost_wjetcr_ele Mww
-# source postfit_loop_Run2_EFT_WV.sh all_run2 boost_wjetcr_ele DNNoutput_boost
-# source postfit_loop_Run2_EFT_WV.sh boostonly_run2 boost_wjetcr_ele DNNoutput_boost -> this fit converged!!! Need to see the postfits and check the eft.sh!!!
-# source postfit_loop_Run2_EFT_WV.sh boostonly_run2 boost_wjetcr_ele Mww
-# source postfit_loop_Run2_EFT_WV.sh boostonly_run2 boost_topcr_ele Mww_binzv 2018
-# source postfit_loop_Run2_EFT_WV.sh boostonly_run2 boost_wjetcr_ele DNNoutput_boost 2016
-# source postfit_loop_Run2_EFT_WV.sh boostonly_run2_rateparam2017 boost_wjetcr_ele DNNoutput_boost 2017
-# source postfit_loop_Run2_EFT_WV.sh boostonly_2018 boost_wjetcr_ele DNNoutput_boost 2018 
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly boost_sig_ele Mww_binzv
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly boost_sig_ele mjj_vbs
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly boost_sig_ele Mww
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly boost_wjetcr_ele DNNoutput_boost
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly boost_wjetcr_ele Mww
+# source postfit_loop_Run2_EFT_WV_env.sh all_run2 boost_wjetcr_ele DNNoutput_boost
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_run2 boost_wjetcr_ele DNNoutput_boost -> this fit converged!!! Need to see the postfits and check the eft.sh!!!
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_run2 boost_wjetcr_ele Mww
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_run2 boost_topcr_ele Mww_binzv 2018
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_run2 boost_wjetcr_ele DNNoutput_boost 2016
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_run2_rateparam2017 boost_wjetcr_ele DNNoutput_boost 2017
+# source postfit_loop_Run2_EFT_WV_env.sh boostonly_2018 boost_wjetcr_ele fit_bins_boost 2018 Mww
 
-Category=$1 #boost
-CUT=$2 #boost_sig_ele
-PLOTVAR=$3 #Mww_binzv
-YEAR=$4
+
 LUMI=0
 echo year "$YEAR"
 if [[ $YEAR -eq 2018 ]]
@@ -78,6 +77,10 @@ echo lumi is ${LUMI}
     #
     #
 echo "${DATACARD_FIT}"
+
+cd 
+source run_VBS_SM_datacard.sh
+
 
 # ->->->->->->->    step - 1a: prepare workspace (done in datacard setup) - here use ALL CATEGORIES to have the proper fit
 
@@ -139,64 +142,70 @@ text2workspace.py ${DatacardPATHpartial}/datacard.txt -o ${DatacardPATHpartial}/
   #  -d ${DatacardPATH}/combined_2018_all_split_Dipole_v4.5_aQGC_cT0.txt \
 
 
+cd 
+source run_VBS_eboliv2.sh
+cd datacards
+
 # ###################################################################
 #         ###      uncomment for postfit  ######
 
 
-# ->->->->->->->    step - 2c: make the postfit (done in analysis setup)
+# # ->->->->->->->    step - 2c: make the postfit (done in analysis setup)
 
-# #    create the folders where to backup files
+#    create the folders where to backup files
 
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${PLOTVAR}
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/
-# mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${PLOTVAR}
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}/${PLOTVAR}
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}
+mkdir -p /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}/${PLOTVAR}
 
-# # # clean local folder
-# rm -r plot_combined/*
+# # clean local folder
+rm -r plot_combined/*
 
-# mkPostFitCombinedPlot.py \
-#   --inputFilePostFitShapesFromWorkspace output_histograms_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
-#   --outputFile output_postfit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
-#   --kind P \
-#   --cutName ${CUT} \
-#   --variable ${PLOTVAR} \
-#   --structureFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/structure_split.py \
-#   --plotFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/plot_split_bins.py \
-#   --lumiText ${LUMI} 
+mkPostFitCombinedPlot.py \
+  --inputFilePostFitShapesFromWorkspace output_histograms_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
+  --outputFile output_postfit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
+  --kind P \
+  --cutName ${CUT} \
+  --variable ${PLOTVAR} \
+  --structureFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/structure_split.py \
+  --plotFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/plot_split_bins.py \
+  --lumiText ${LUMI} 
  
-# # # ->->->->->->->    step - 2d: make the postfit plot (done in analysis setup)
+# # ->->->->->->->    step - 2d: make the postfit plot (done in analysis setup)
 
-# mkPlot.py --pycfg=configuration_combined.py --inputFile=output_postfit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root --onlyPlot=cratio --logOnly --showIntegralLegend=1 --minLogCratio=0.1 --maxLogCratio=10000
+mkPlot.py --pycfg=configuration_combined.py --inputFile=output_postfit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root --onlyPlot=cratio --logOnly --showIntegralLegend=1 --minLogCratio=0.1 --maxLogCratio=10000
 
-# cp -r plot_combined/*png /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${PLOTVAR}/
+cp -r plot_combined/*png /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/postfit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}/${PLOTVAR}/
 
 
-# # ###################################################################
-# #         ###      uncomment for prefit   ######
+# ###################################################################
+#         ###      uncomment for prefit   ######
 
-# # # ->->->->->->->    step - 2e: make the prefit (done in analysis setup)
+# # ->->->->->->->    step - 2e: make the prefit (done in analysis setup)
 
-# mkPostFitCombinedPlot.py \
-#   --inputFilePostFitShapesFromWorkspace output_histograms_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
-#   --outputFile output_prefit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
-#   --kind p \
-#   --cutName ${CUT} \
-#   --variable ${PLOTVAR} \
-#   --structureFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/structure_split.py \
-#   --plotFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/plot_split_bins.py \
-#   --lumiText ${LUMI}
+mkPostFitCombinedPlot.py \
+  --inputFilePostFitShapesFromWorkspace output_histograms_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
+  --outputFile output_prefit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root \
+  --kind p \
+  --cutName ${CUT} \
+  --variable ${PLOTVAR} \
+  --structureFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/structure_split.py \
+  --plotFile ../Full${YEAR}v7/conf_fit_v4.5_aQGC/plot_split_bins.py \
+  --lumiText ${LUMI}
 
-# #     # clean up local plotter folder
-# rm -r plot_combined/*
+#     # clean up local plotter folder
+rm -r plot_combined/*
 
-# # ->->->->->->->    step - 2f: make the prefit plot (done in analysis setup)
+# ->->->->->->->    step - 2f: make the prefit plot (done in analysis setup)
 
-# mkPlot.py --pycfg=configuration_combined.py --inputFile=output_prefit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root --onlyPlot=cratio --logOnly --showIntegralLegend=1 --minLogCratio=0.1 --maxLogCratio=10000
+mkPlot.py --pycfg=configuration_combined.py --inputFile=output_prefit_${YEAR}_EFT_WV_${fulloperator}_${CUT}.root --onlyPlot=cratio --logOnly --showIntegralLegend=1 --minLogCratio=0.1 --maxLogCratio=10000
 
-# cp -r plot_combined/*png /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${PLOTVAR}/
+cp -r plot_combined/*png /eos/user/i/izoi/VBS_SM_WV_semilep_aQGC/prefit/WV_${YEAR}/${fulloperator}/${Category}/${CUT}/${SRVAR}/${PLOTVAR}/
 
