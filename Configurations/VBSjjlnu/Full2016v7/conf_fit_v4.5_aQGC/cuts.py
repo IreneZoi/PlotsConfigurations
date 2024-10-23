ff6dc475d87d3af1ff95e7c8c60595fef2a6458c
IreneZoi <irene.zoi@cern.ch>
2024-10-23 16:45:44 +0200

    commented resolved categories

--- Configurations/VBSjjlnu/Full2016v7/conf_fit_v4.5_aQGC/cuts.py
+++ Configurations/VBSjjlnu/Full2016v7/conf_fit_v4.5_aQGC/cuts.py
@@ -16,25 +16,25 @@ supercut = '(   (abs(Lepton_pdgId[0])==11 && Lepton_pt[0]>30)\
 ############ 
 ## Signal
 
-cuts["res_sig_ele"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==11 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet > 65 && mjj_vjet < 105 \
-                                && bVeto \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_sig_ele"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==11 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet > 65 && mjj_vjet < 105 \
+#                                 && bVeto \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
 
 
-cuts["res_sig_mu"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==13 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet > 65 && mjj_vjet < 105 \
-                                && bVeto \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_sig_mu"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==13 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet > 65 && mjj_vjet < 105 \
+#                                 && bVeto \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
 cuts["boost_sig_ele"] = 'VBS_category==0 \
                             && abs(Lepton_pdgId[0])==11 \
@@ -55,25 +55,25 @@ cuts["boost_sig_mu"] = 'VBS_category==0 \
 ###############
 ##### Wjets
 
-cuts["res_wjetcr_ele"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==11 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet> 40 && (mjj_vjet <= 65 || mjj_vjet >= 105) \
-                                && bVeto \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_wjetcr_ele"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==11 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet> 40 && (mjj_vjet <= 65 || mjj_vjet >= 105) \
+#                                 && bVeto \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
 
 
-cuts["res_wjetcr_mu"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==13 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet> 40 && (mjj_vjet <= 65 || mjj_vjet >= 105) \
-                                && bVeto \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_wjetcr_mu"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==13 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet> 40 && (mjj_vjet <= 65 || mjj_vjet >= 105) \
+#                                 && bVeto \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
 cuts["boost_wjetcr_ele"] = 'VBS_category==0 \
                             && abs(Lepton_pdgId[0])==11 \
@@ -96,23 +96,23 @@ cuts["boost_wjetcr_mu"] = 'VBS_category==0 \
 
 ### Top Tight region
 
-cuts["res_topcr_ele"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==11 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet > 65 && mjj_vjet < 105 \
-                                && bReqTight \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_topcr_ele"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==11 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet > 65 && mjj_vjet < 105 \
+#                                 && bReqTight \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
-cuts["res_topcr_mu"] = 'VBS_category==1 \
-                                && abs(Lepton_pdgId[0])==13 \
-                                && vjet_0_pt > 30 && vjet_1_pt > 30 \
-                                && mjj_vjet > 65 && mjj_vjet < 105 \
-                                && bReqTight \
-                                && w_had_pt < 200 \
-                                && veto_fatjet_180 \
-                                '
+# cuts["res_topcr_mu"] = 'VBS_category==1 \
+#                                 && abs(Lepton_pdgId[0])==13 \
+#                                 && vjet_0_pt > 30 && vjet_1_pt > 30 \
+#                                 && mjj_vjet > 65 && mjj_vjet < 105 \
+#                                 && bReqTight \
+#                                 && w_had_pt < 200 \
+#                                 && veto_fatjet_180 \
+#                                 '
 
 
 # Tight top
