b379a1022f7c740bfc3f4edc702c1b6f81455933
IreneZoi <irene.zoi@cern.ch>
2024-10-23 16:44:06 +0200

    run on all operators and signal samples

--- Configurations/VBSjjlnu/scripts/nuisances_tools/extract_nuisance_norm.py
+++ Configurations/VBSjjlnu/scripts/nuisances_tools/extract_nuisance_norm.py
@@ -9,7 +9,7 @@ The effect is saved as the ratio of variationn/nominal.
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--input", help="Input file", type=str)
 parser.add_argument("-o","--output", help="Output file", type=str)
-parser.add_argument("-s","--samples", help="Samples", type=str, nargs="+")
+# parser.add_argument("-s","--samples", help="Samples", type=str, nargs="+")
 parser.add_argument("-c","--cut", help="Cut", type=str)
 parser.add_argument("-n","--nuisances", help="Nuisances", type=str, nargs="+")
 args = parser.parse_args()
@@ -24,7 +24,14 @@ output = {}
 
 cut = args.cut
 print "Cut: ", cut
-for s in args.samples:
+theOperators = ["cT0", "cT2", "cT1", "cT3", "cT4", "cT5", "cT6", "cT7", "cT8", "cT9", "cS0", "cS1", "cS2", "cM0", "cM1", "cM2", "cM3", "cM4", "cM5", "cM6", "cM7", "cM8", "cM9"]
+full_operators_name = []
+for op in theOperators:
+     full_operators_name.append("quad_"+op)
+     full_operators_name.append("sm_lin_quad_"+op)
+full_operators_name.append("sm")
+
+for s in full_operators_name:
     output[s] = {}
     print ">> Sample: ", s 
     h_nom = iF.Get("{}/events/histo_{}".format(cut, s ))
